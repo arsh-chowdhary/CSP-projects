cc848d88bff9f5c0cf4cdeb9582ae140d087dad6
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-09-10 20:24:13 +0000

    Finished expression practice python

--- Expression Practice/expressionPython.py
+++ Expression Practice/expressionPython.py
@@ -7,3 +7,13 @@ numSix = 6
 numSeven = 7
 numEight = 8
 numNine = 9
+
+print(numSeven - 24 / numEight * numFour + numSix)
+print(18 / numThree - numSeven + numTwo * numFive)
+print(numSix * numFour / 12 + 72 / numEight - numNine)
+print((17 - numSix / numTwo) + numFour * numThree)
+print(-2 * (numOne * numFour - numTwo / numTwo) + (numSix + numTwo - numThree))
+print(-1 * ((numThree - numFour * numSeven) / numFive) - 2 * 24 / numSix)
+print((numThree * numFive**2 / 15) - (numFive - numTwo**2))
+print((numOne**4 * numTwo**2 + numThree**3) - numTwo**5 / numFour)
+print((22 / numTwo - numTwo * 5)**2 + (numFour - numSix / numSix)**2)
