7fe524ab374f028de1fe7153998d4ea01792e139
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-09-11 20:42:20 +0000

    started expression C almost done

--- Expression Practice/expressionC.c
+++ Expression Practice/expressionC.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
-    int numberTwo[1] = 2;
-    int numberSix[1] = 6;
-    printf("%f", 7 - 24 / 8 * 4 + numberSix);
+    int numberTwo = 2;
+    int numberSix = 6;
+    float powerOne = pow(5,2); // 5 to the power of 2
+    float powerTwo = pow(2,2);
+    float powerThree = pow(1, 4);
+    float powerFour = pow(3, 3);
+    float powerFive = pow(2, 5);
+    printf("%d \n", 7 - 24 / 8 * 4 + numberSix);
+    printf("%d \n", 18 / 3 - 7 + numberTwo * 5);
+    printf("%d \n", numberSix * 4 / 12 + 72 / 8 - 9);
+    printf("%d \n", (17 - 6 / numberTwo) + 4 * 3);
+    printf("%d \n", -2 * (1 * 4 - numberTwo / numberTwo) + (numberSix + numberTwo - 3));
+    printf("%d \n", -1 * ((3 - 4 * 7) / 5) -2 * 24 / numberSix);
+    printf("%f \n", (3 * powerOne / 15) - (5 - powerTwo));
+    printf("%d \n", (powerThree * [powerTwo + powerFour] -));
+
     return 0;
     
 }
