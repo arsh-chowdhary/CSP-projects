be3a046f81ff3974ef00938c7ee42101daf5f48f
arsh-chowdhary <arsh.chowdhary@ucas-edu.net>
2024-10-11 19:25:57 +0000

    fizzbuzz c

--- FizzBuzz/fizzbuzz.c
+++ FizzBuzz/fizzbuzz.c
@@ -4,23 +4,16 @@ int i;
 int main(){
     // Create a loop that counts to 50
     for(i=1;i<=50;i++){
-        
-    
-        // Replace #'s divisible by 3 and 5 with "FizzBuzz"
         if(i%3==0 && i%5==0){
             printf("FizzBuzz\n");
-        // Replace #'s divisible by 3 with "Fizz"
         }else if (i%3==0){
-            printf("Fizz")
-        // Replace #'s divisible by 3 with "Fizz"
-
-        // Replace #'s divisible by 5 with "Buzz"
+            printf("Fizz\n");
+        }else if (i%5==0){
+            printf("Buzz\n");
         }else
-        // Print all other numbers
-
-        printf("%d\n", i);
+            printf("%d\n", i);
         }
-    }
+
     return 0;
     
 }
